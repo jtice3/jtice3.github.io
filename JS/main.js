763d4695ee9982a4f259e0e6c803df7655ede0d6
jtice3 <37914914+jtice3@users.noreply.github.com>
2018-12-04 15:25:05 -0700

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+// Animate Smooth Scroll 
+$('#viewWork').on('click', function() {
+    const images = $('#images').position().top;
+
+    $('html, body').animate({
+        scrollTop: images
+    }, 900);
+});
